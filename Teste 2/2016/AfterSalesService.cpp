516126b4d89cb8a6056138c76a7b8a3e1a56c6d6
DanielRodrigues202006562 <up202006562@up.pt>
2021-12-08 12:31:20 +0000

    More tests

--- Teste 2/2016/AfterSalesService.cpp
+++ Teste 2/2016/AfterSalesService.cpp
@@ -43,10 +43,15 @@ void AfterSalesService::dropPresentsOnTable(vector<Article*> presents) {
  * Remove from the table all articles of a given client.
  */
 vector<Article*> AfterSalesService::pickPresentsFromTable(long client) {
-
-	// TODO
-
-	vector<Article*> clientPresents;
+    vector<Article*> clientPresents;
+    vector<Article*> aux;
+    for(auto idx: presentsTable){
+        if(idx->getClient() == client)
+            clientPresents.push_back(idx);
+        else
+            aux.push_back(idx);
+    }
+    presentsTable = aux;
 	return clientPresents;
 }
 
@@ -57,9 +62,34 @@ vector<Article*> AfterSalesService::pickPresentsFromTable(long client) {
  *       the one with lower present number should have priority (clients should not wait too much);
  * (iii) else, maintain article order in the table.
  */
-void AfterSalesService::sortArticles() {
+bool MySort1(const Article* a1, const Article* a2){
+    if(a1->getDeliverHome() && !a2->getDeliverHome())
+        return true;
+    return false;
+}
 
-	// TODO
+bool mySort(const Article* a1, const Article* a2){
+    if(a1->getPresentNumber() + 2 < a2->getPresentNumber())
+        return true;
+    return a1->getArrivalNumber() < a2->getArrivalNumber();
+}
+
+void AfterSalesService::sortArticles() {
+    vector<Article*> aux = {presentsTable.begin(), presentsTable.begin()+10};
+    vector<Article*> auxHome;
+
+    for(auto it = aux.begin(); it != aux.end(); it++) {
+        if ((*it)->getDeliverHome()) {
+            auxHome.push_back(*it);
+            it = --aux.erase(it);
+        }
+    }
+    sort(aux.begin(),aux.end(),mySort);
+    aux.insert(aux.end(),auxHome.begin(),auxHome.end());
+
+    for(size_t i = 0; i < 10; i++){
+        presentsTable[i] = aux[i];
+    }
 
 }
 
@@ -67,19 +97,26 @@ void AfterSalesService::sortArticles() {
  * Move the first presents in the table to the queue of presents to wrap, taking into account the size of the queue.
  */
 void AfterSalesService::enqueueArticles() {
-
-	// TODO
-
+    int count = 0;
+    while (toWrap.size() < toWrapQueueSize && presentsTable.size()>count){
+        toWrap.push(presentsTable[count]);
+        count++;
+    }
+    presentsTable.erase(presentsTable.begin(), presentsTable.begin()+count);
 }
 
 /**
  * Process the first present to wrap. If it is an article to deliver home, the article is placed in the toDeliver queue.
  */
 Article* AfterSalesService::wrapNext() {
-
-	// TODO
-
-	return 0;
-
+    if(toWrap.size() == 0)
+        return NULL;
+    Article* aux = toWrap.front();
+    toWrap.pop();
+    if(aux -> getDeliverHome()) {
+        toDeliver.push(aux);
+        return NULL;
+    }
+    return aux;
 }
 
--- Teste 2/2016/Purchase.cpp
+++ Teste 2/2016/Purchase.cpp
@@ -5,6 +5,7 @@
  *      Author: hlc
  */
 
+#include <queue>
 #include "Purchase.h"
 
 using namespace std;
@@ -24,10 +25,11 @@ list< stack<Article*> > Purchase::getBags() const {
  * Create an Article associated with the client of this purchase.
  */
 Article* Purchase::createArticle(long barCode, bool present, bool deliverHome) {
+    Article* a1 = new Article (client, barCode);
+    a1->setPresent(present);
+    a1->setDeliverHome(deliverHome);
 
-	// TODO
-
-	return 0;
+	return a1;
 
 }
 
@@ -35,9 +37,14 @@ Article* Purchase::createArticle(long barCode, bool present, bool deliverHome) {
  * Add an Article to the bags of this purchase. A new bag is added when the last bag is full.
  */
 void Purchase::putInBag(Article* article) {
-
-	// TODO
-
+    if(bags.back().size() < BAG_SIZE && bags.size() != 0){
+        bags.back().push(article);
+    }
+    else{
+        stack<Article*> temp;
+        temp.push(article);
+        bags.push_back(temp);
+    }
 }
 
 /**
@@ -45,11 +52,28 @@ void Purchase::putInBag(Article* article) {
  * All other articles are pushed back into the bags where they were, maintaining order.
  */
 vector<Article*> Purchase::popPresents() {
-
-	// TODO
-
 	vector<Article*> presents;
-	return presents;
+    for(auto it = bags.begin(); it != bags.end(); it++){
+        stack<Article*> aux;
+        while(!it->empty()){
+            if(it->top()->getPresent())
+                presents.push_back(it->top());
+            else
+                aux.push(it->top());
+            it->pop();
+        }
+        if(aux.size() == 0) {
+            bags.erase(it++);
+            continue;
+        }
+        else{
+            while (!aux.empty()){
+                it->push(aux.top());
+                aux.pop();
+            }
+        }
 
+    }
+	return presents;
 }
 
